05ffa73d6f13a42432a550671f563a7c934460b3
Alex Berthon <alexberthon@alexs-macbook-pro.local>
2016-08-29 17:43:50 -0700

    Initial Commit

---
+++ Lab1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab1
+//
+//  Created by Alex Berthon on 8/29/16.
+//  Copyright © 2016 Alex Berthon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
